744a9474e824c4ea785d79f2b3fe0afe0dd1ea3b
imora1 <imora1@uwyo.edu>
2024-11-25 03:27:13 +0000

    Lab10

--- cosc1010-lab10.py
+++ cosc1010-lab10.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Isabell Mora
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# 11-24--24
+# Lab 10
+# Lab Section:11
+# Sources, people worked with, help given to: Ethan Romero
 # your
 # comments
 # here
@@ -16,6 +16,31 @@ from pathlib import Path
 def get_hash(to_hash):
     """You can use """
     return sha256(to_hash.encode('utf-8')).hexdigest().upper()
+def get_password():
+    try:
+        with open('hash', 'r') as hash: 
+            hash_storage = hash.read().strip()
+    except FileNotFoundError:
+        print("File 'hash' was not found.")
+        return 
+    except OSError:
+        print("Error opening the the 'hash' file.")
+        return
+    
+    try:
+        with open('rockyou.txt','r') as rockyou_file:
+            for password in rockyou_file:
+                password = password.strip()
+                hash_password = get_hash(password)
+                if hash_password == hash_storage:
+                    print(f"Password: {password}")
+                    return
+    except FileNotFoundError:
+        print("'rockyou.txt' was not found")
+    except OSError:
+        print("Error opeing the 'rockyou.txt' file.")
+get_password()
+
 
 
 
